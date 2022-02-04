21a4c3cd4e0adb7b076e99dbe3ab2368bd2eca02
Sangwoo Shin <sangwoonoel@naver.com>
2022-02-04 18:36:12 +0900

    [feat] BrandLike Model

--- brand/models.py
+++ brand/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import *
 
+
+
+
+class BrandLike(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True)
 # Create your models here.
+
