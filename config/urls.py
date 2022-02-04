4387a033fcaf3d03065c9d226e916a99f6554d14
Shin won seok <tkddls23@konkuk.ac.kr>
2022-02-05 01:23:42 +0900

    [fix] rename community

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('brand/', include('brand.urls')),
-    path('comunity/', include('community.urls')),
+    path('post/', include('post.urls')),
     path('', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
