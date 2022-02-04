13dd7a066bec86e41a954582bf99607f3da0a1c0
Jihyun Kim <ang21icjh@gmail.com>
2022-02-04 18:38:59 +0900

    [feat] User/Brand model

--- brand/models.py
+++ brand/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import *
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField("브랜드명", max_length=20)
+    # category
+    image = models.ImageField("브랜드 이미지", upload_to="brand/")
+    info = models.TextField("브랜드 소개")
+    link = models.URLField("브랜드 링크")
