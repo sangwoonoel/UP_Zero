41a3b5d3f004864c18383b4d0604bff0a80b2054
Suehyun Kim <sue555@naver.com>
2022-02-06 03:59:37 +0900

    [feat] community CRUD and like ajax

---
+++ static/js/post/like.js
@@ -0,0 +1,60 @@
+const post = document.querySelector(".post");
+const postLiked = post.getAttribute("data-liked");
+const likeBtn = post.querySelector(".post-like__btn");
+
+if (postLiked === "true") {
+    likeBtn.style.color = "red";
+}
+
+const onClickAlert = () => {
+    alert("로그인이 필요합니다.");
+};
+
+const requestLike = new XMLHttpRequest();
+const onClickLike = (userID, postID) => {
+    const post = document.querySelector(`.post[data-id="${postID}"]`);
+    const postLiked = post.getAttribute("data-liked");
+
+    let action;
+    if (postLiked === "true") {
+        action = "off";
+        post.setAttribute("data-liked", "false");
+    } else {
+        action = "on";
+        post.setAttribute("data-liked", "true");
+    }
+
+    const url = "/post/like/";
+    requestLike.open("POST", url, true);
+    requestLike.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+    );
+    requestLike.send(
+        JSON.stringify({ user_id: userID, post_id: postID, action: action })
+    );
+    };
+
+    const likeHandler = () => {
+    if (requestLike.status < 400) {
+        const { action } = JSON.parse(requestLike.response);
+        const likeBtn = document.querySelector(".post-like__btn");
+        const likeCnt = document.querySelector(".post-like__cnt");
+        let num;
+
+        if (action === "on") {
+            likeBtn.style.color = "red";
+            num = Number(likeCnt.innerText) + 1;
+        } else {
+            likeBtn.style.color = "black";
+            num = Number(likeCnt.innerText) - 1;
+        }
+        likeCnt.innerText = num;
+    }
+};
+
+requestLike.onreadystatechange = () => {
+    if (requestLike.readyState === XMLHttpRequest.DONE) {
+        likeHandler();
+    }
+};
