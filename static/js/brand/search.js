fa0eff146cd686688e8da38ad66cb01abc888a7d
Jihyun Kim <ang21icjh@gmail.com>
2022-02-06 06:24:37 +0900

    [fix] no search results case

--- static/js/brand/search.js
+++ static/js/brand/search.js
@@ -1,12 +1,18 @@
 const keyword = document.querySelector(".keyword");
 keyword.addEventListener("keydown", (e) => {
   if (e.keyCode !== 13) return;
-  onClickSearch(); // 엔터 치면 onClickSearch 실행
+  onClickSearch(); // 엔터 쳐도 onClickSearch 실행
 });
 
 const requestSearch = new XMLHttpRequest();
 const onClickSearch = () => {
   const keyword = document.querySelector(".keyword");
+
+  if (!keyword.value) {
+    // 검색어 없으면 브랜드 기본 리스트 페이지로 이동
+    location.href = "/brand/list";
+    return;
+  }
 
   const url = "/brand/search/";
   requestSearch.open("POST", url, true);
@@ -28,7 +34,7 @@ const searchResHandler = () => {
     keyword = keyword.toLowerCase();
 
     const newURL = `?keyword=${keyword}`;
-    history.replaceState(null, null, newURL);
+    history.replaceState(null, null, newURL); // Querystring 형태로 URL 변경 (99% 완성..?)
 
     // if (typeof history.pushState != "undefined") {
     //   let baseURL = location.href.includes("?")
@@ -40,19 +46,27 @@ const searchResHandler = () => {
     // }
 
     const brandListSection = document.querySelector("#brand-list");
+
     const existingSearchResult = document.querySelector(".search-result");
     if (existingSearchResult) {
       existingSearchResult.remove();
-    }
+    } // 기존 검색 결과 안내 있었다면 삭제
     const searchResult = document.createElement("p");
-    searchResult.classList.toggle("search-result");
-    searchResult.innerHTML = `<span class="search-result__keyword">${keyword}</span>에 대한
-    <span class="search-result__cnt">${brands.length}</span>개의 브랜드 검색
-    결과입니다.`;
-    brandListSection.prepend(searchResult);
+    searchResult.classList.toggle("search-result"); // 새로운 검색 결과 안내
 
     const brandsContainer = document.querySelector(".brands");
-    brandsContainer.innerHTML = "";
+    brandsContainer.innerHTML = ""; // 브랜드 리스트 초기화
+
+    if (brands.length === 0) {
+      searchResult.innerHTML = "검색 결과가 없습니다.";
+      brandListSection.prepend(searchResult);
+      return;
+    }
+
+    searchResult.innerHTML = `<span class="search-result__keyword">${keyword}</span>에 대한
+      <span class="search-result__cnt">${brands.length}</span>개의 브랜드 검색
+      결과입니다.`;
+    brandListSection.prepend(searchResult);
 
     for (const brand of brands) {
       const brandContainer = document.createElement("div");
