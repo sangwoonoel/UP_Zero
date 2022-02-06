df91d9ed934841e11dc4df890f120575f68948ba
Shin won seok <tkddls23@konkuk.ac.kr>
2022-02-07 02:55:07 +0900

    [feat] post-search

--- post/views.py
+++ post/views.py
@@ -6,28 +6,28 @@ import json
 from django.http import JsonResponse
 
 
-
 def post_detail(request, pk):
     post = Post.objects.get(id=pk)
     comments = Comment.objects.all()
     like_cnt = PostLike.objects.filter(post=post).count()
 
-    try: # 로그인 한 상태일 때
+    try:  # 로그인 한 상태일 때
         if PostLike.objects.filter(user=request.user, post=post).exists():
             is_liked = True
         else:
             is_liked = False
-    
-    except TypeError: # 로그인 안 한 상태일 때       
+
+    except TypeError:  # 로그인 안 한 상태일 때
         is_liked = False
-    ctx = {'post':post, 'comments':comments, "is_liked":is_liked, 'like_cnt':like_cnt}
+    ctx = {'post': post, 'comments': comments,
+           "is_liked": is_liked, 'like_cnt': like_cnt}
 
     return render(request, template_name='post/post_detail.html', context=ctx)
 
 
 def post_create(request):
     if request.method == "POST":
-        form = PostForm(request.POST, request.FILES) 
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save()
             # post.user = request.user
@@ -35,31 +35,31 @@ def post_create(request):
             return redirect('post:post_detail', pk=post.pk)
     else:
         form = PostForm()
-    ctx = {'form':form}
+    ctx = {'form': form}
 
     return render(request, template_name='post/post_form.html', context=ctx)
 
 
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
-  
+
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES, instance=post)
-        
+
         if form.is_valid():
             post = form.save()
             return redirect('post:post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
 
-        ctx = {'form':form, 'post':post}
+        ctx = {'form': form, 'post': post}
         return render(request, template_name='post/post_form.html', context=ctx)
 
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
-    return redirect('post:post_list')
+    return redirect('post:list')
 
 
 @csrf_exempt
@@ -78,7 +78,7 @@ def like_post(request):
         post_like = get_object_or_404(PostLike, user=user, post=post)
         post_like.delete()
 
-    return JsonResponse({"action":action})
+    return JsonResponse({"action": action})
 
 
 def post_list(request):
@@ -91,7 +91,6 @@ def search_for_posts(request):
     req = json.loads(request.body)  # need to learn how to deserialize queryset
     keyword = req["keyword"]
     posts = Post.objects.filter(title__icontains=keyword)
-    print('hello')
 
     # 각 brand object를 dictionary 형태로 변환
     return JsonResponse({"keyword": keyword, "posts": list(posts.values())})
--- static/js/post/search.js
+++ static/js/post/search.js
@@ -6,7 +6,6 @@ keyword.addEventListener("keydown", (e) => {
 
 
 const onClickSearch = async(keyword) => {
-    console.log(keyword.value)
     if (!keyword.value) {
         // 검색어 없으면 포스트 기본 리스트 페이지로 이동
         location.href = "/post/list";
@@ -22,4 +21,80 @@ const onClickSearch = async(keyword) => {
 }
 
 const searchResHandler = (keyword,posts) => {
+    keyword = keyword.toLowerCase();
+
+    const newURL = `?keyword=${keyword}`;
+    history.replaceState(null, null, newURL); // Querystring 형태로 URL 변경 (99% 완성..?)
+
+    // if (typeof history.pushState != "undefined") {
+    //   let baseURL = location.href.includes("?")
+    //     ? location.href.split("?")[0]
+    //     : location.href;
+    //   baseURL = baseURL.slice(0, -1); // Remove trailing slash
+    //   const newURL = baseURL + `?keyword=${keyword}`;
+    //   history.pushState({ path: newURL }, null, newURL);
+    // }
+
+    const postListSection = document.querySelector("#post-list");
+
+    const existingSearchTitle = document.querySelector(".post-search-title");
+    if (existingSearchTitle) {
+      existingSearchTitle.remove();
+    } // 기존 검색 결과 안내 있었다면 삭제
+    const searchTitle = document.createElement("p");
+    searchTitle.classList.toggle("post-search-title"); // 새로운 검색 결과 안내
+
+    const existingPostsContainer = document.querySelector(".posts");
+    if (existingPostsContainer) {
+      existingPostsContainer.remove();
+    } // 기존 브랜드 리스트 있었다면 삭제
+
+    if (posts.length === 0) {
+      // 검색 결과 없을 때
+      searchTitle.innerHTML = "검색 결과가 없습니다.";
+      postListSection.prepend(searchTitle);
+      return;
+    }
+
+    // 검색 결과 존재 시
+    searchTitle.innerHTML = `<span class="post-search-title__keyword">${keyword}</span>에 대한
+      <span class="post-search-title__cnt">${posts.length}</span>개의 게시물 검색
+      결과입니다.`;
+    postListSection.prepend(searchTitle);
+
+    const postsContainer = document.createElement("div");
+    postsContainer.classList.toggle("posts"); // 새로운 브랜드 리스트
+    postListSection.append(postsContainer);
+
+    for (const post of posts) {
+      const postContainer = document.createElement("div");
+      postContainer.classList.toggle("post");
+      postContainer.innerHTML = `<a href="{% url 'post:post_detail' post.id %}">
+      <div class="post-title">
+          제목: ${post.title}
+      </div>
+      <div class="post-preview">
+          ${post.content}
+      </div>
+      <div class="post-author">
+          작성자 : {{post.user.username}}
+      </div>
+      <div class="post-created">
+          <span>작성시간 : </span>
+          {% if post.created_string == False %}
+          <td>{{ post.created_at|date:'m월 d일' }}</td> 
+          {% else %}
+          <td>{{ post.created_string }}</td>
+          {% endif %}
+      </div>
+      <div class="post-comment">
+          댓글 : {{post.comment_set.count}}개
+      </div>
+      <div class="post-like">
+          좋아요 : {{post.postlike_set.count}}개
+      </div>
+  </a>`; 
+      postsContainer.append(postContainer);
+    }
 }
+
