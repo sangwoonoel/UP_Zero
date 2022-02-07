b3d3748de5e61a4550467911cd0fe1e3f8cb2384
Jihyun Kim <ang21icjh@gmail.com>
2022-02-08 03:56:50 +0900

    [feat] share on kakaotalk

--- static/js/share.js
+++ static/js/share.js
@@ -12,9 +12,46 @@ $(document).ready(function () {
 
 const title = document.querySelector(".brand__name").innerText;
 const url = location.href;
+const imageUrl = document.querySelector(".brand__img").src;
 
 document.querySelector("meta[property='og:url']").setAttribute("content", url);
+document.querySelector("meta[property='og:image']").setAttribute("content", imageUrl);
 document.querySelector("meta[name='twitter:url']").setAttribute("content", url);
+document.querySelector("meta[name='twitter:image']").setAttribute("content", imageUrl);
+
+
+Kakao.init("173a3e42a028e54899bcec0a78b03d66");
+const shareKakao = () => {
+  Kakao.Link.sendDefault({
+    objectType: "feed",
+    content: {
+      title: `${title}`,
+      description: `${document.querySelector(".brand__desc").innerText}`,
+      imageUrl: `${imageUrl}`,
+      link: {
+        webUrl: `${url}`,
+        // mobileWebUrl: `${url}`
+      },
+    },
+    social: {
+      likeCount: Number(document.querySelector(".like-cnt").innerText),
+    },
+    buttons: [
+      {
+        title: "자세히 보기",
+        link: {
+          webUrl: `${url}`,
+        },
+      },
+      // {
+      //   title: '모바일',
+      //   link: {
+      //     mobileWebUrl: '${url}',
+      //   },
+      // },
+    ],
+  });
+};
 
 const shareFacebook = () => {
   open(`http://www.facebook.com/sharer.php?u=${url}&t=${title}`);
