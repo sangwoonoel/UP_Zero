fe20d0bacd9f66738f11ff1808d923cfbf330c57
Jihyun Kim <ang21icjh@gmail.com>
2022-02-08 00:47:31 +0900

    [feat] kakao share icon and brand fields

--- brand/models.py
+++ brand/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from users.models import *
 
+class Category(models.Model):
+    name = models.CharField('카테고리명', max_length=20)
+
+    def __str__(self):
+        return self.name
+
 class Brand(models.Model):
     name = models.CharField('브랜드명', max_length=20)
-    # category
+    category = models.ForeignKey(Category, verbose_name='카테고리', on_delete=models.SET_NULL, null=True)
+    desc = models.CharField('브랜드 소개', max_length=100)
+    info = models.TextField('브랜드 설명')
     image = models.ImageField('브랜드 이미지', upload_to='brand/')
-    info = models.TextField('브랜드 소개')
     link = models.URLField('브랜드 링크')
 
     def __str__(self):
