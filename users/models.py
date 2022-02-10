e57a38b062dde4eed4a3b69bb8514246accac3d3
Jihyun Kim <ang21icjh@gmail.com>
2022-02-11 02:49:27 +0900

    [feat] add an image field to user model & edit html to show the user profile image

--- users/models.py
+++ users/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    nickname = models.CharField("닉네임", max_length=10, unique=True, null=True)
-    email = models.EmailField("이메일", unique=True)
+    nickname = models.CharField('닉네임', max_length=10, unique=True, null=True)
+    email = models.EmailField('이메일', unique=True)
+    image = models.ImageField('프로필 이미지', default='logo.png', upload_to='user/', null=True)
