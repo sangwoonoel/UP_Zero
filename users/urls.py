a86f622209b78c84673d3c2986a75d7a979713bf
Sangwoo Shin <sangwoonoel@naver.com>
2022-02-11 17:27:27 +0900

    [feat]user_reset-password

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'users'
 
@@ -11,8 +12,14 @@ urlpatterns = [
     path("mypage/", views.mypage, name="mypage"),
     path("user_post/", views.user_post, name="user_post"),
     path("post_delete/", views.mypage_post_delete, name="mypage_post_delete"),
-    path("brand_delete/", views.mypage_brand_delete, name="mypage_brand_delete"),
+    path("brand_delete/", views.mypage_brand_delete, name="mypage_brand_delete"),    
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+     
     # path("user_post_delete/", views.user_post_delete, name="user_post_delete"),
+
 
 
     
