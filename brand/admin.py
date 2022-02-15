8a707234d59f7be47c0505c9073fbb5310eeed18
Jihyun Kim <ang21icjh@gmail.com>
2022-02-16 03:05:31 +0900

    [chore] minor css changes of brand list/search & increase max length of post title

--- brand/admin.py
+++ brand/admin.py
@@ -6,7 +6,6 @@ class BrandAdmin(admin.ModelAdmin):
     list_display = ['name', 'category']
     search_fields = ['name', 'category__name']
 @admin.register(BrandLike)
-
 class BrandLikeAdmin(admin.ModelAdmin):
     search_fields = ['user__username', 'user__nickname', 'brand__name']
 admin.site.register(Category)
