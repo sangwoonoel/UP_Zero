420469928a84ae60bd2f69ee5795e10e47064482
Shin won seok <tkddls23@konkuk.ac.kr>
2022-02-18 00:32:00 +0900

    [design] created.js

--- static/js/post/created.js
+++ static/js/post/created.js
@@ -3,11 +3,22 @@ for (let creat of created) {
     time = creat.innerText
     if (time.slice(-3) === '분 전')
     {
-        creat.innerText = '\u00a0\u00a0'+ time;
+        if(time.length === 4){
+            creat.innerText = '\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0'+ time;
+            console.log(time)
+        }
+        else {
+            creat.innerText = '\u00a0\u00a0\u00a0\u00a0'+ time;
+        }
     }
     else if (time.slice(-3) === '일 전')
     {
-        creat.innerText = '\u00a0\u00a0\u00a0\u00a0'+ time;
+        creat.innerText = '\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0'+ time;
+
+    }
+    else if (time.slice(-4) === '방금 전')
+    {
+        creat.innerText = '\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0'+ time;
 
     }
 }
